262b1b29310488e8c4c896a0e25fa2d3ee67ffbf
arpit999 <arpitpatel009@gmail.com>
2016-06-24 17:01:00 +0530

    Initial commit

---
+++ app/src/main/java/com/hometech/customexpandable/MainActivity.java
@@ -0,0 +1,101 @@
+package com.hometech.customexpandable;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.ExpandableListView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends Activity {
+
+    private ExpandListAdapter ExpAdapter;
+    private ArrayList<Group> ExpListItems;
+    private ExpandableListView ExpandList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ExpandList = (ExpandableListView) findViewById(R.id.exp_list);
+        ExpListItems = SetStandardGroups();
+        ExpAdapter = new ExpandListAdapter(MainActivity.this, ExpListItems);
+        ExpandList.setAdapter(ExpAdapter);
+
+    }
+
+    public ArrayList<Group> SetStandardGroups() {
+
+        String group_names[] = {"Group A", "Group B", "Group C", "Group D",
+                "Group E", "Group F", "Group G", "Group H"};
+
+        String country_names[] = {"Brazil", "Mexico", "Croatia", "Cameroon",
+                "Netherlands", "chile", "Spain", "Australia", "Colombia",
+                "Greece", "Ivory Coast", "Japan", "Costa Rica", "Uruguay",
+                "Italy", "England", "France", "Switzerland", "Ecuador",
+                "Honduras", "Agrentina", "Nigeria", "Bosnia and Herzegovina",
+                "Iran", "Germany", "United States", "Portugal", "Ghana",
+                "Belgium", "Algeria", "Russia", "Korea Republic"};
+
+        int Images[] = {
+                R.mipmap.ic_launcher,
+                R.mipmap.ic_launcher,
+                R.mipmap.ic_launcher,
+                R.mipmap.ic_launcher,
+                R.mipmap.ic_launcher,
+                R.mipmap.ic_launcher,
+                R.mipmap.ic_launcher,
+                R.mipmap.ic_launcher,
+                R.mipmap.ic_launcher,
+                R.mipmap.ic_launcher,
+                R.mipmap.ic_launcher,
+                R.mipmap.ic_launcher,
+                R.mipmap.ic_launcher,
+                R.mipmap.ic_launcher,
+                R.mipmap.ic_launcher,
+                R.mipmap.ic_launcher,
+                R.mipmap.ic_launcher,
+                R.mipmap.ic_launcher,
+                R.mipmap.ic_launcher,
+                R.mipmap.ic_launcher,
+                R.mipmap.ic_launcher,
+                R.mipmap.ic_launcher,
+                R.mipmap.ic_launcher,
+                R.mipmap.ic_launcher,
+                R.mipmap.ic_launcher,
+                R.mipmap.ic_launcher,
+                R.mipmap.ic_launcher,
+                R.mipmap.ic_launcher,
+                R.mipmap.ic_launcher,
+                R.mipmap.ic_launcher,
+                R.mipmap.ic_launcher,
+                R.mipmap.ic_launcher
+        };
+
+        ArrayList<Group> list = new ArrayList<Group>();
+
+        ArrayList<Child> ch_list;
+
+        int size = 4;
+        int j = 0;
+
+        for (String group_name : group_names) {
+            Group gru = new Group();
+            gru.setName(group_name);
+
+            ch_list = new ArrayList<Child>();
+            for (; j < size; j++) {
+                Child ch = new Child();
+                ch.setName(country_names[j]);
+                ch.setImage(Images[j]);
+                ch_list.add(ch);
+            }
+            gru.setItems(ch_list);
+            list.add(gru);
+
+            size = size + 4;
+        }
+
+        return list;
+    }
+}
